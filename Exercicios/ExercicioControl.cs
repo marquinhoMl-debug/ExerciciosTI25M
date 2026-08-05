81febaafa03cf06854f687d4c52da68728c8cf63
ALLAN SOBRAL DA SILVA <allan.ssilva4@senacsp.edu.br>
2026-08-05 08:06:20 -0300

    Primeiro Commit

---
+++ Exercicios/ExercicioControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    class ExercicioControl
+    {
+        ExercicioModel modelo;//Conectando a Classe Model e Control
+
+        public ExercicioControl()
+        {
+            this.modelo = new ExercicioModel(); 
+        }//Construtor Padrão
+
+        public void Coletar()
+        {
+            //Peguei os dois
+            Console.Write("Informe o primeiro número: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Informe o segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Vou passar para o construtor com parâmetro
+            this.modelo = new ExercicioModel(num1, num2);
+        }//fim do coletar = Pega os dois números digitados
+
+
+
+    }//fim da classe
+}//fim do projeto
---
+++ Exercicios/ExercicioModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios
+{
+    class ExercicioModel
+    {
+        //Área para declaração de variáveis globais
+        private int num1;
+        private int num2;
+        private int resultado;
+
+        //Método Construtor: Instância as variáveis na memória
+        public ExercicioModel()
+        {
+            SetNum1(0);
+            SetNum2(0);
+            SetResultado(0);
+        }//fim do método construtor
+
+        public ExercicioModel(int num1, int num2)
+        {
+            SetNum1(num1);
+            SetNum2(num2);
+            SetResultado(0);
+        }//fim do construtor
+
+        //Métodos GETS E SETS
+        //GET = Pegar, Obter, Consultar
+        //SET = Alterar, Modificar, Configurar
+        public int GetNum1()
+        {
+            return this.num1;
+        }//fim do getNum1
+
+        public void SetNum1(int num1)
+        {
+            this.num1 = num1;
+        }//fim do setNum1
+
+        public int GetNum2()
+        {
+            return this.num2;
+        }//fim do getNum2
+
+        public void SetNum2(int num2)
+        {
+            this.num2 = num2;
+        }//fim do setNum2
+
+        public int GetResultado()
+        {
+            return this.resultado;
+        }//fim do método
+
+        public void SetResultado(int resultado)
+        {
+            this.resultado = resultado;
+        }//fim do método setResultado
+
+        public int Somar()
+        {
+            return this.num1 + this.num2;
+        }//fim do método somar
+
+        public int Subtrair()
+        {
+            SetResultado(GetNum1() - GetNum2());
+            return GetResultado();
+        }//fim do método subtrair
+
+        public int Multiplicar()
+        {
+            SetResultado(GetNum1() * GetNum2());
+            return GetResultado(); 
+        }//fim do multiplicar
+
+        public double Dividir()
+        {
+            if(GetNum2() <= 0)
+            {
+                return -1;
+            }
+            else
+            {
+                SetResultado(GetNum1() / GetNum2());
+                return GetResultado();
+            }//fim do if
+        }//fim do dividir
+
+
+
+    }//fim da classe
+}//fim do projeto
